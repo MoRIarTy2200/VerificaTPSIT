74f05103cf45143a31174eb61d11a124ef6138f8
Sarcinella <manuel@DESKTOP-L56JHPI>
2020-05-13 10:46:01 +0200

    versione 0

---
+++ src/SarcinellaClass.java
@@ -0,0 +1,43 @@
+
+/**
+ * 
+ * @author Manuel <b>Sarcinella</b> - <u>4AI</u> - <i>13/05/2020</i>
+ *
+ */
+
+public class SarcinellaClass {
+
+	/**
+	 * @author Manuel Sarcinella
+	 */
+	public SarcinellaClass() {
+		
+	}
+	
+	
+	/**
+	 * @param b primo numero
+	 * @param e secondo numero
+	 * 
+	 * @return valore
+	 */
+	public float cognomeMet (int b, int e) {
+		int val1=1; 		
+			for (int i=1; i<=b; i++){
+				val1 = val1 * b;
+			}
+			return val1;
+		}
+	
+	
+	/**
+	 * @author Manuel Sarcinella
+	 */
+	public static void main(String[] args) {
+		SarcinellaClass s=new SarcinellaClass();
+		
+		System.out.println(s.cognomeMet(3, 10));
+
+	}
+
+}
